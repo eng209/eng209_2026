849e85390b8ba7bf97bdbe54ceb7bf6a26109791
Eric Bouillet <eric.bouillet@epfl.ch>
2026-08-14 15:21:13 +0200

    install falls back to setup folder

--- setup.sh
+++ setup.sh
@@ -2,19 +2,52 @@
 
 # Linux/macOS wrapper for setup.py
 # Finds python3.12 and creates the class project folder with a virtualenv inside.
-# Defaults to ~/Desktop/myfiles; pass a folder in first argument to override.
+# Defaults to ~/Desktop/myfiles or a parent folder of script; pass a folder in first argument to override.
 # Made for ENG209 VM; may need adjustments on other systems.
 
 main() {
 	set -euC
-	local -r COURSE_FOLDER="${1-${HOME}/Desktop/myfiles}"
-	local -r PYTHON_PATH="${PYTHON_PATH-/opt/python3.12.2/bin/python3.12}"
-	local -r SETUP_URL="https://raw.githubusercontent.com/eng209/assets/refs/heads/main/tools/setup.py"
 
-	if [[ ! -e "${COURSE_FOLDER}" ]]; then
-		echo "${COURSE_FOLDER} does not exist. Try: ./setup.sh /path/to/folder"
-		return 1
+	local -r DEFAULT_COURSE_FOLDER="${HOME}/Desktop/myfiles"
+	local -r PYTHON_PATH="${PYTHON_PATH:-/opt/python3.12.2/bin/python3.12}"
+	local -r SETUP_URL="https://raw.githubusercontent.com/eng209/assets/refs/heads/main/tools/setup.py"
+	local -r SETUP_CMD=$(realpath -q "$0")
+
+	if [[ "${SETUP_CMD:-}" =~ ^.*eng209_[0-9]{4}[/\]setup.sh$ ]]; then
+		# Install may fall back to setup's folder unless it is in a git repository
+		if [[ ! -d $(dirname "${SETUP_CMD}")/.git ]]; then
+			local -r SETUP_FOLDER=$(dirname $(dirname "${SETUP_CMD}"))
+		fi
 	fi
+
+	case "${1-}" in
+		-h|--help) cat <<- EOF
+			Usage: $0 [/path/to/folder]
+
+			If not specified the folder default to ${DEFAULT_COURSE_FOLDER}, or a parent of setup scripts.
+			The folder must exist and be writable.
+		EOF
+		return ;;
+	esac
+
+	local COURSE_FOLDER="${1:-}"
+
+	if [[ -n "${COURSE_FOLDER-}" ]]; then
+		if [[ ! -d "${COURSE_FOLDER}" ]]; then
+			echo "${COURSE_FOLDER} does not exist or is not a folder. Try: ./setup.sh /path/to/folder"
+			return
+		fi
+	elif [[ -d "${DEFAULT_COURSE_FOLDER}" ]]; then
+		COURSE_FOLDER="${DEFAULT_COURSE_FOLDER}"
+	elif [[ -n "${SETUP_FOLDER:-}" ]]; then
+		COURSE_FOLDER=${SETUP_FOLDER}
+	else
+		echo "Cannot identify a target folder. Try: ./setup.sh /path/to/folder"
+		return
+	fi
+
+	COURSE_FOLDER=$(realpath "${COURSE_FOLDER}")
+	touch "${COURSE_FOLDER}" || return
 
 	for python_exe in ${PYTHON_PATH} $(type -a -P -- python3.12 python3); do
 		if [[ $("${python_exe}" --version 2>&1) =~ ^Python\ 3\.12 ]]; then
@@ -25,7 +58,7 @@ main() {
 
 	if [[ ! -x "${PYTHON_EXE-}" ]]; then
 		echo "Python 3.12 not found. Try: PYTHON_PATH=/path/to/python.exe ./setup.sh"
-		return 1
+		return
 	fi
 
 	"${PYTHON_EXE}" -- <<-EOF
